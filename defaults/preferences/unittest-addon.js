7881ade168dbb9dca6bdaef7642f38d3f3e48da1
TiagoRibeiro <tiago.sistema@yahoo.com>
2016-02-09 14:46:43 -0200

    Configurações para o addon.

--- defaults/preferences/unittest-addon.js
+++ defaults/preferences/unittest-addon.js
@@ -1,1 +1,4 @@
-pref('extensions.unittest-addon.namespace', '');
+pref('extensions.unittest-addon.timeCheck', 500);
+pref('extensions.unittest-addon.namespace', 'UNITTEST');
+pref('extensions.unittest-addon.module', '');
+pref('extensions.unittest-addon.host', 'http://');
