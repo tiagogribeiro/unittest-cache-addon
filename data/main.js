b9d7a5c268d49124ad38f20127b6aca477b39e1a
TiagoRibeiro <tiago.sistema@yahoo.com>
2016-02-14 22:02:50 -0200

    Arquivo que manipula a tela de resultados.

---
+++ data/main.js
@@ -0,0 +1,43 @@
+var panel = {
+        addModule : function (oJson, isLeft) {
+                var divModule = "";
+                var divPanel = "";
+                var li = "";
+
+                divModule = document.createElement("div");
+                divModule.className = ((isLeft == true) ? "module-div left" : "module-div");				
+
+                li = document.createElement("li");
+                li.className ="text";
+                li.innerHTML = "Modulo: " + oJson.module;
+                divModule.appendChild(li);
+
+                li = document.createElement("li");
+                li.className ="text";
+                li.innerHTML = "Testes: " + oJson.test;
+                divModule.appendChild(li);
+
+                li = document.createElement("li");
+                li.className ="text";
+                li.innerHTML = "Erros: " + oJson.error;
+                divModule.appendChild(li);
+
+                divPanel = document.getElementById("unittest-panel");
+                divPanel.appendChild(divModule);
+
+        }, 
+
+        // @param json: {"list":[{"module":"est", "test":"10", "error":"2"}, {"module":"fin", "test":"5", "error":"0"}]}
+        load : function (json) {				
+                var isLeft = "";
+                var oList = JSON.parse(json);
+
+                for (var i=0;i<oList.list.length;i++) {
+                        isLeft = ((i%2) != 0);
+                        this.addModule(oList.list[i], isLeft);
+                } 
+        }	
+}
+
+
+
