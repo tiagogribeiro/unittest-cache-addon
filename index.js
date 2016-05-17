74f6d1b29f326a2fd73e1295854a4083483592e4
Lucas AF <lucas.af88@outlook.com>
2016-05-16 23:46:49 -0300

    Implementado login e separado a rotina de resultado em um arquivo na pasta lib

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 "use strict";
 var self = require("sdk/self");
+var resultTest = require("./lib/result.js");
 
-var { ToggleButton } = require("sdk/ui/button/toggle");
-  var panel = require("sdk/panel").Panel({
-    contentURL: self.data.url("unittest-panel.html"),
-    onHide: handleHide,
+// Preferences
+var preference = {
+      urlTest: "http://10.1.1.3:57772/csp/sys/%25UnitTest.Portal.Home.zen"
+}
+
+var panel = require("sdk/panel").Panel({
+	 contentURL: self.data.url("unittest-panel.html"),
+    onHide: handleHide,    
     height: 305
-  //scontentScriptFile: self.data.url("api.js")
 });
 
+panel.port.on("login-submit", function (text) {
+	panel.contentURL = self.data.url("unittest-panel.html");
+	hiddenFrame.element.contentWindow.location = preference.urlTest;
+});
 
+var { ToggleButton } = require("sdk/ui/button/toggle");
 var buttonIcon = ToggleButton({
     id: "unittest-button-icon",
     label: "UnitTest",
@@ -25,7 +34,7 @@ function handleChange(state) {
     if (state.checked) {
         panel.show({
             position: buttonIcon
-        });
+        });        
     }
 }
 
@@ -33,98 +42,21 @@ function handleHide() {
     buttonIcon.state('window', {checked: false});
 }
 
-//------------------------------------------
-// Implementando a lógica dos resultados.
-var NIVEL_ULTIMO = 4;
-var NIVEL_RESULTADO = 3;
-var _countResultados = 0;
-var _results = [];
-var _linhaDeResultado = null;
-
-// Definição das configurações.
-var preference = {
-    urlTest: "resource://unittest-cache-addon/html/Pagina1.html"
-}
-
-
 // Iframe Hidden.
 var hiddenFrames = require("sdk/frame/hidden-frame");
 var hiddenFrame = hiddenFrames.add(hiddenFrames.HiddenFrame({
     onReady: function () {
         this.element.contentWindow.location = preference.urlTest;
-        var self = this;
-        this.element.addEventListener("DOMContentLoaded", function () {
-            //console.log(self.element.contentDocument.body);
-            _monteResultado(self.element.contentDocument.body);
-        }, true, true);
+        var frame = this;
+        this.element.addEventListener("DOMContentLoaded", function () {        	   
+        	   
+	    		var requiresLogin = (frame.element.contentDocument.title.indexOf("Login") > -1);
+            if (requiresLogin) {
+                panel.contentURL = self.data.url("unittest-login.html");
+            } else {
+            	resultTest.monteResultado(frame.element.contentDocument.body);
+            }
+            
+        }, true, true);       
     }
 }));
-
-function _isUltimoNivelResultado(){
-  return (_countResultados === NIVEL_ULTIMO);
-}
-
-function _isNivelResultadoTest(){
-  return (_countResultados === NIVEL_RESULTADO);
-}
-
-function _resultadoDeSucess(){
-  return (/^success$/g.test(_linhaDeResultado.innerHTML()) ? true : false);
-}
-
-function _resultadoDeFalha(){
-  return (/^failed$/g.test(_linhaDeResultado.innerHTML()) ? true : false);
-}
-
-function _moduloDoResultado(){
-  var result = _linhaDeResultado.innerHTML().split('.')[1];
-  if (result==='')
-      throw "Sem definição do módulo do resultado";
-  return result;
-}
-
-function _proximaPaginaDeResultado(){
-  _linhaDeResultado.child().click();
-}
-
-function _existeMaisResultados(){
-  return (_linhaDeResultado.length > 0);
-}
-
-function _listResultados(){
-  return _linhaDeResultado.getElementsById("tpNum");
-
-}
-
-function _incrementaContagemResultado(){
-    _countResultados++;
-}
-function _setResultado( body ){
-  _linhaDeResultado = body.getElementsById('tpAction');
-}
-
-function _monteResultado( body ) {
-
-    _setResultado( body );
-
-    if ( _isUltimoNivelResultado() )
-        return false;
-
-    if ( _isNivelResultadoTest() ){
-        for ( var indice in _listResultados() ) {
-            _linhaDeResultado = _listResultados()[indice];
-            _results.push({
-                modulo: _moduloDoResultado(),
-                success: _resultadoDeSucess(),
-                failed: _resultadoDeFalha()
-            });
-            _incrementaContagemResultado();            
-        }
-        console.log( "Resultados:" + _results );
-    } else if ( _existeMaisResultados() ) {
-          _incrementaContagemResultado();
-          _proximaPaginaDeResultado();
-          // Sem necessidade de chamar novamente para montar pois o próprio controle do frame
-          //  irá disparar novamente este método.
-    }
-}
